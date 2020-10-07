6cb8c3af2c742c819d6bc264c2e76f0ce22f53e2
munish2301 <munishkumar22595@gmail.com>
2020-10-07 21:00:48 +0530

    Adding API to access covid-data

---
+++ break_the_chain/lib/networking.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Network {
+  final String url;
+  Network({this.url});
+  Future getData() async {
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print("Status Code = $response.statusCode");
+    }
+  }
+}

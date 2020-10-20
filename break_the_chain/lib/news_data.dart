722271eee57d3fae18aa247dc18923194f378516
RajanBhagat08 <bhagatrajan562@gmail.com>
2020-10-20 09:52:14 +0530

    Fetching news Articles

--- break_the_chain/lib/news_data.dart
+++ break_the_chain/lib/news_data.dart
@@ -1,2 +1,13 @@
+import 'networking.dart';
+
 const newsAPIIndia =
     "http://newsapi.org/v2/top-headlines?category=health&apiKey=71ad51a827344c628fbde1ba273af6be";
+
+class NewsDataModel {
+  Future<dynamic> getNews() async {
+    Network netIndia = Network(url: newsAPIIndia);
+    dynamic indiaNews = await netIndia.getData();
+    dynamic articles = indiaNews["articles"];
+    return articles;
+  }
+}

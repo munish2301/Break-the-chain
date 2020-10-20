cca8dc110a2606b9380a14a9983ceaaa5b301c22
prathusingal <prathusingal@gmail.com>
2020-10-20 20:34:29 +0530

    Connecting loading screen with news screen

--- break_the_chain/lib/loadingscreen2.dart
+++ break_the_chain/lib/loadingscreen2.dart
@@ -1,4 +1,10 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'news_data.dart';
+import 'newsdatascreen.dart';
+import 'nointernet.dart';
 
 class LoadingScreen2 extends StatefulWidget {
   static String loadingScreenId2 = "LoadingScreenId2";
@@ -8,10 +14,48 @@ class LoadingScreen2 extends StatefulWidget {
 
 class _LoadingScreen2State extends State<LoadingScreen2> {
   @override
+  void initState() {
+    super.initState();
+    getnews();
+  }
+
+  void getnews() async {
+    bool result = await DataConnectionChecker().hasConnection;
+    if (result == false) {
+      Navigator.pushNamed(context, NoInternetScreen.noInternetScreenID);
+      return;
+    }
+    NewsDataModel nm = NewsDataModel();
+    var data = await nm.getNews();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return NewsDataScreen(
+            newsdata: data,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[50],
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitDoubleBounce(
+              color: Colors.teal[400],
+              size: 100.0,
+            ),
+            Text(
+              'Only I and You can Break the Chain!',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

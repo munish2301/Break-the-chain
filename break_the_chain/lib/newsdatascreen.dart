173f6570b3f3757946c160f3684dd02fdcb1136f
RajanBhagat08 <bhagatrajan562@gmail.com>
2020-10-20 15:46:05 +0530

    Adding News Screen

---
+++ break_the_chain/lib/newsdatascreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+import 'homepage.dart';
+import 'news_data.dart';
+
+NewsDataModel cm = NewsDataModel();
+
+class NewsDataScreen extends StatefulWidget {
+  final dynamic newsdata;
+  NewsDataScreen({@required this.newsdata});
+  @override
+  _NewsDataScreenState createState() => _NewsDataScreenState();
+}
+
+class _NewsDataScreenState extends State<NewsDataScreen> {
+  dynamic newsArticles;
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.of(context).pushNamed(HomePage.homepageId);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            iconSize: 35.0,
+            onPressed: () {
+              Navigator.pushNamed(context, HomePage.homepageId);
+            },
+          ),
+          backgroundColor: Colors.teal,
+          title: Text(
+            "Health News",
+            style: kCovidDataTitleTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}

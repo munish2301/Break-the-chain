1a917abc5df6156d9c2067a45898e208f0c10f1b
thakurkeshav <thakurkeshav612@gmail.com>
2020-10-21 14:37:19 +0530

    Improving UI of newstile

--- break_the_chain/lib/newstile.dart
+++ break_the_chain/lib/newstile.dart
@@ -36,35 +36,52 @@ class NewsTile extends StatelessWidget {
         }
       },
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        alignment: Alignment.bottomCenter,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(
-                urlToImage,
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
+        margin: EdgeInsets.only(bottom: 24),
+        width: MediaQuery.of(context).size.width,
+        child: Container(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(6),
+                    bottomLeft: Radius.circular(6))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: Image.network(
+                      urlToImage == null
+                          ? "https://cdn.dribbble.com/users/71890/screenshots/2368143/health_news.jpg"
+                          : urlToImage,
+                      height: 200,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    )),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  title == null ? "" : title,
+                  maxLines: 2,
+                  style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  description == null ? "" : description,
+                  maxLines: 3,
+                  style: TextStyle(color: Colors.teal[300], fontSize: 14),
+                )
+              ],
             ),
-            Text(
-              title,
-              maxLines: 2,
-              style: TextStyle(
-                  color: Colors.teal,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500),
-            ),
-            Text(
-              description,
-              maxLines: 3,
-              style: TextStyle(color: Colors.teal[300], fontSize: 14),
-            )
-          ],
+          ),
         ),
       ),
     );

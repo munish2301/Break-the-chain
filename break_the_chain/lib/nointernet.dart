361083b177ec136ce76e6671a673bf644ca9ed44
thakurkeshav <thakurkeshav612@gmail.com>
2020-10-23 12:23:27 +0530

    Improving UI of No Internet screen

--- break_the_chain/lib/nointernet.dart
+++ break_the_chain/lib/nointernet.dart
@@ -13,11 +13,28 @@ class NoInternetScreen extends StatelessWidget {
         return Future.value(false);
       },
       child: Scaffold(
+        backgroundColor: Colors.teal[50],
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.teal[50],
+                radius: 80.0,
+                child: Image.asset(
+                  "images/internet.png",
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
             Center(
               child: Text(
                 "Please check your internet connection!",
+                style: TextStyle(color: Colors.teal),
               ),
             ),
           ],
